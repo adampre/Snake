7176a82d0c57205c6a14c841ab91fcac8b9596d1
A-1 pressler <ohyes1356@outlook.com>
2022-12-21 00:15:51 -0800

    Code to associate ints with enum values.

--- SnakeCode/Direction.java
+++ SnakeCode/Direction.java
@@ -1,9 +1,38 @@
 package SnakeCode;
 
+import java.util.Map;
+import java.util.HashMap;
+
 public enum Direction 
 {
-    Up, 
-    Left, 
-    Right, 
-    Down,
+    Up(0), 
+    Left(1), 
+    Right(2), 
+    Down(3);
+
+    private int value;
+    private static Map<Integer, Direction> map = new HashMap<Integer, Direction>();
+
+    private Direction(int value)
+    {
+        this.value = value;
+    }
+
+    static 
+    {
+        for(Direction direction : Direction.values())
+        {
+            map.put(direction.value, direction);
+        }
+    }
+
+    public static Direction valueOf(int direction)
+    {
+        return (Direction)map.get(direction);
+    }
+
+    public int getValue()
+    {
+        return value;
+    }
 }
